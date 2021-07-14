455c6a51104a3b673144db292628a0d09ab5f329
KhanTagiev <xan.2011@yandex.ru>
2021-07-15 00:23:04 +0300

    feat: CORS

---
+++ backend/middlewares/cors.js
@@ -0,0 +1,28 @@
+const app = require('../app');
+
+const allowedCors = [
+  'https://domainname.khantagiev.nomoredomains.club',
+  'http://domainname.khantagiev.nomoredomains.club',
+  'localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const requestHeaders = req.headers['access-control-request-headers'];
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+  }
+  next();
+});
